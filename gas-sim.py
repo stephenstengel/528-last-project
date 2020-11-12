84a9a1187dd8897e9224ca8f60a1491ffd94a7c2
Stephen <stephen.stengel@cwu.edu>
2020-11-12 03:48:41 -0800

    Added wall collision. A little slow though.

--- gas-sim.py
+++ gas-sim.py
@@ -11,6 +11,7 @@
 import numpy
 import matplotlib.pyplot as plt
 import random
+import math
 
 def main(args):
 	print("Hi!")
@@ -37,12 +38,12 @@ def main(args):
 	numpoints = 100
 	# ~ numpoints = 10
 	
-	numSteps = 100
+	numSteps = 100000
 	
 	#Get xhigh up to wall
 	wallstop = getxhighAbutment(xsize, xlow)
 	
-	print("wallstop: " + str(wallstop) + "\txlow: " + str(xlow) + "\txsize: " + str(xsize))
+	# ~ print("wallstop: " + str(wallstop) + "\txlow: " + str(xlow) + "\txsize: " + str(xsize))
 	
 	#Create array to hold x and y positions of particles
 	xpositions = numpy.random.randint(xlow, wallstop, numpoints)
@@ -55,10 +56,10 @@ def main(args):
 	
 	xwallArray, ywallArray = createWallArray(xsize, ysize, xlow, xhigh, ylow, yhigh)
 	
-	print(xpositions)
-	print(ypositions)
+	# ~ print(xpositions)
+	# ~ print(ypositions)
 	
-	simulate(numSteps, xpositions, ypositions, xwallArray, ywallArray, xlow, xhigh, ylow, yhigh)
+	simulate(numSteps, xpositions, ypositions, xwallArray, ywallArray, xlow, xhigh, ylow, yhigh, ysize)
 	
 	return 0
 
@@ -84,10 +85,12 @@ def createWallArray(xsize, ysize, xlow, xhigh, ylow, yhigh):
 	ywallArray = []
 	
 	wallxpos = getxhighAbutment(xsize, xlow)
-	print("wallxpos: " + str(wallxpos))
+	# ~ print("wallxpos: " + str(wallxpos))
 	
-	yFirstThird = (ysize // 3) + ylow
-	ySecondThird = ((ysize * 2) // 3) + ylow
+	# ~ yFirstThird = (ysize // 3) + ylow
+	# ~ ySecondThird = ((ysize * 2) // 3) + ylow
+	
+	yFirstThird, ySecondThird = yPartitions(ysize, ylow)
 	
 	for i in range(ylow, yhigh + 1):
 		if i <= yFirstThird or i >= ySecondThird:
@@ -104,13 +107,90 @@ def createWallArray(xsize, ysize, xlow, xhigh, ylow, yhigh):
 	return numpy.array(xwallArray), numpy.array(ywallArray)
 
 
+#Returns the y partition points for the wall.
+def yPartitions(ysize, ylow):
+	yFirstThird = (ysize // 3) + ylow
+	ySecondThird = ((ysize * 2) // 3) + ylow
+	
+	return yFirstThird, ySecondThird
+
 #runs the simulation for some number of steps.
-def simulate(numSteps, xpositions, ypositions, xwallArray, ywallArray, xlow, xhigh, ylow, yhigh):
-	#print initial condition.
+def simulate(numSteps, xpositions, ypositions, xwallArray, ywallArray, xlow, xhigh, ylow, yhigh, ysize):
+	#print initial condition
+	printSimulation(xpositions, ypositions, xwallArray, ywallArray, xlow, xhigh, ylow, yhigh, 0)
+	
+	movesArray = numpy.random.randint(1, 5, size=(numSteps, len(xpositions)))
+	
+	NORTH = 1
+	EAST = 2
+	SOUTH = 3
+	WEST = 4
+	
+	for step in range(1, numSteps):
+		# ~ print(movesArray[step])
+		#random walk the arrays.
+		# ~ print("xpos b4 west")
+		# ~ print(xpositions)
+		xpositions -= numpy.where(movesArray[step] == WEST, 1, 0)
+		# ~ print("xpos after west")
+		# ~ print(xpositions)
+		
+		xpositions += numpy.where(movesArray[step] == EAST, 1, 0)
+		ypositions += numpy.where(movesArray[step] == NORTH, 1, 0)
+		ypositions -= numpy.where(movesArray[step] == SOUTH, 1, 0)
+		
+		#fix error points.
+		xpositions, ypositions = fixInWall(xpositions, ypositions, xwallArray, ywallArray, xlow, xhigh, ylow, yhigh, ysize)
+		
+		#print updated field
+		if step % 10000 == 0:
+			printSimulation(xpositions, ypositions, xwallArray, ywallArray, xlow, xhigh, ylow, yhigh, step)
+
+
+#Moves points to a good position if they impact walls.
+def fixInWall(xpositions, ypositions, xwallArray, ywallArray, xlow, xhigh, ylow, yhigh, ysize):
+	#Fix out of bounds
+	for i in range(len(xpositions)):
+		if xpositions[i] < xlow:
+			xpositions[i] = xlow
+		elif xpositions[i] > xhigh:
+			xpositions[i] = xhigh
+		
+		if ypositions[i] < ylow:
+			ypositions[i] = ylow
+		elif ypositions[i] > yhigh:
+			ypositions[i] = yhigh
+	
+	#fix wall collision
+	#get wall coords
+	leftSideWall = math.inf
+	rightSideWall = -math.inf
+	
+	yFirstThird, ySecondThird = yPartitions(ysize, ylow) #move up functions and pass?
 	
 	
-	for step in range(numSteps):
-		printSimulation(xpositions, ypositions, xwallArray, ywallArray, xlow, xhigh, ylow, yhigh, step)
+	for i in range(len(xwallArray)):#move up functions loops
+		if xwallArray[i] < leftSideWall:
+			leftSideWall = xwallArray[i]
+		if xwallArray[i] > rightSideWall:
+			rightSideWall = xwallArray[i]
+	
+	#for each xy coord,
+	for i in range(len(xpositions)):
+		# ~ #If at a wall place
+		# ~ #left
+		if xpositions[i] == leftSideWall:
+			#if not in free middle third y section.
+			if ypositions[i] <= yFirstThird or ypositions[i] >= ySecondThird:
+				xpositions[i] -= 1
+		
+		#right
+		elif xpositions[i] == rightSideWall:
+			if ypositions[i] <= yFirstThird or ypositions[i] >= ySecondThird:
+				xpositions[i] += 1
+		
+	
+	return xpositions, ypositions
 
 
 def printSimulation(xArray, yArray, xwallArray, ywallArray, xlow, xhigh, ylow, yhigh, currentStep):
