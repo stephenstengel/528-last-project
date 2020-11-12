c8b22abcb23d489ac5066a36b621bf7abde758b4
Stephen <stephen.stengel@cwu.edu>
2020-11-11 23:54:12 -0800

    Beginning project.

---
+++ gas-sim.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#Stephen Stengel  cs528  Final Project
+#Simulation of gas particles with partial wall
+
+
+def main(args):
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
